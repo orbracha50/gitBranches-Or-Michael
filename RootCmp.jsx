c8a0a910f480b9086420be151932ac9de0d98a07
MichaelSpiridonov <michael.sp579@gmail.com>
2024-07-08 15:57:22 +0300

    Fix Store Issue & Add ContactEdit Route

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -6,8 +6,11 @@ import { ContactDetails } from "./pages/ContactDetails.jsx"
 import { Homepage } from "./pages/Homepage.jsx"
 import { ContactIndex } from "./pages/ContactIndex.jsx"
 import { AppFooter } from "./cmps/AppFooter.jsx"
+import { store } from './store/store.js'
+import { ContactEdit } from './pages/ContactEdit.jsx'
 export function App() {
   return (
+    <Provider store={store}>
     <Router>
         <AppHeader />
         <main>
@@ -15,9 +18,12 @@ export function App() {
             <Route element={<Homepage />} path={"/"} />
             <Route element={<ContactDetails />} path={"/contact/:contactId"} />
             <Route element={<ContactIndex />} path={"/contact"} />
+            <Route element={<ContactEdit />} path={"/contact/edit"} />
+            <Route element={<ContactEdit />} path={"/contact/edit/:contactId"} />
           </Routes>
         </main>
         <AppFooter />
     </Router>
+    </Provider>
   )
 }
